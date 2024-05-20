01d03afd376aed0b87034086065760321be54599
h31w1tty <heiwitty.16@gmail.com>
2024-05-20 19:38:24 -0300

    conexao com utf8

--- conexao.php
+++ conexao.php
@@ -4,6 +4,7 @@ $usuario = "root";
 $senha = "usbw";
 $banco = "midiasispumi";
 $con = new mysqli($servidor, $usuario, $senha, $banco);
+mysqli_set_charset($con, "utf8");
 if(!$con){
     echo'falha ao conectar!';
 }
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   $sql2 = 'SELECT * FROM noticia WHERE status_noticia = 1 ORDER BY id_noticia DESC LIMIT 3'; //listar ultimas 3 noticias ativas
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
 	<meta charset="utf-8">
   <meta http-equiv="Cache-Control" content="no-store" /> <!--  REMOVER -->
