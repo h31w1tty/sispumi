523380fafceff3c67083e3cd8aaa845d6474ad7f
h31w1tty <heiwitty.16@gmail.com>
2024-06-05 21:35:58 -0300

    icones das paginas

--- painel-carrossel.php
+++ painel-carrossel.php
@@ -162,6 +162,16 @@
         </aside>
         <!-- ↑ NAVEGAÇÃO LATERAL ↑ -->
 
+        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-body">
+                Hello, world! This is a toast message.
+                <div class="mt-2 pt-2 border-top">
+                <button type="button" class="btn btn-primary btn-sm">Take action</button>
+                <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="toast">Close</button>
+                </div>
+            </div>
+        </div>
+        
         <div class="col-sm-1" style="background-color: #fafbfe;"></div>
         <div class="main p-3">
             <br>
